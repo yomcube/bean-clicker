278e084c7257a2bd35e3796a08d7982d9e6715e9
TomCube <121310847+yomcube@users.noreply.github.com>
2023-03-09 23:54:34 -0600

    change number to string

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var beans = document.getElementById("beans").innerText;
 var amountToYield = 1;
 function click() {
-    beans = parseInt(beans) + amountToYield;
+    var a = parseInt(beans) + amountToYield;
+    beans = a.toString();
 }
