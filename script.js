c819b9a3dba542d799aae4e11a8ea7893e3cbc4f
TomCube <121310847+yomcube@users.noreply.github.com>
2023-03-11 09:44:19 -0600

    major changes

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 var beans = 0;
-var amount = 1;
+var amountPerClick = 1;
+var beansPerSecond = 0;
+var buildings = {
+    farm: {
+        name: "Farm", cost: 75, bPS: 1, count: 0,
+        buy: function() {
+            buildings.farm.count += 1;
+            buildings.farm.cost += buildings.farm.cost * 0.15;
+            beansPerSecond += buildings.farm.bPS;
+        }
+    }
+}
 function clickbean() {
-    beans += amount;
+    beans += amountPerClick;
     document.getElementById("beans").innerText = beans.toString();
 }
+function update() {
+    beans += beansPerSecond;
+}
+setInterval(update, 1000);
